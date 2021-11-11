9207f5a3355e11cb775befe211cff35ef213d7ae
kureyan <yuruzo-uresiine@hotmail.co.jp>
2021-11-11 13:12:11 +0900

    Bullet

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+public class Player : MonoBehaviour
+{
+
+	public float speed = 4f; //歩くスピード
+	public GameObject bullet;
+	private Rigidbody2D newrigidbody2D;
+	private Animator anim;
+
+	void Start()
+	{
+		//各コンポーネントをキャッシュしておく
+		anim = GetComponent<Animator>();
+		newrigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void Update()
+    {
+		if (Input.GetKeyDown("left ctrl"))
+		{
+			anim.SetTrigger("Shot");
+			Instantiate(bullet, transform.position + new Vector3(0f, 0f, 0f), transform.rotation);
+		}
+	}
+
+    void FixedUpdate()
+	{
+		//左キー: -1、右キー: 1
+		float x = Input.GetAxisRaw("Horizontal");
+		//左か右を入力したら
+		if (x != 0)
+		{
+			//入力方向へ移動
+			newrigidbody2D.velocity = new Vector2(x * speed, newrigidbody2D.velocity.y);
+			//localScale.xを-1にすると画像が反転する
+			Vector2 temp = transform.localScale;
+			temp.x = x;
+			transform.localScale = temp;
+			//Wait→Dash
+			anim.SetBool("Dash", true);
+			//左も右も入力していなかったら
+		}
+		else
+		{
+			//横移動の速度を0にしてピタッと止まるようにする
+			newrigidbody2D.velocity = new Vector2(0, newrigidbody2D.velocity.y);
+			//右向きにする
+			Vector2 temp = transform.localScale;
+			temp.x = 1;
+			transform.localScale = temp;
+			//Dash→Wait
+			anim.SetBool("Dash", false);
+		}
+	}
+}
